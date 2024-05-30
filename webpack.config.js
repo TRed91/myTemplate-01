dbdfaf1dad8c69431a7e49c92f719196a832381f
Thomas Roth <thomas.roth@aon.at>
2024-05-30 16:31:06 +0200

    setup template

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const { type } = require('os');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Document',
+          template: './src/index.html'
+        }),
+      ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|gif|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|otf|eot|ttf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.toml$/i,
+                type: 'json',
+                parser: {
+                  parse: toml.parse,
+                },
+              },
+              {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                  parse: yaml.parse,
+                },
+              },
+              {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                  parse: json5.parse,
+                },
+              },
+        ],
+    },
+};
